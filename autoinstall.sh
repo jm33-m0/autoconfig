cfccad55a01d6dfe2fc7e99cde8817b5acf39be3
jm33 <jm33.m0@gmail.com>
2016-02-17 20:22:51 +0800

    It's working now...

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,10 +4,10 @@ then
     echo "You are not root !"
     exit 1
 fi
-#pacman -Syyu
+pacman -Syyu
 wait
-printf "\n\nPreparing to install software from official repo..."
-#pacman -S iw wpa_supplicant git zsh wipe vim alsa xorg-server xf86-input-evdev xf86-input-synaptics xf86-video-intel android-tools android-udev htop linux-headers dkms i3 xfce4-terminal dmenu sddm networkmanager intel-ucode leafpad mtr nmapnet-tools pcmanfm feh wayland openssh  plasma chromium firefox hexchat libreoffice gimp keepnote virtualbox virtualbox-host-modules virtualbox-ext-oracle virtualbox-guest-iso virtualbox-host-dkms qt4 wget axel whois
+printf "\n\nPreparing to install software from official repo...\n\n"
+pacman -S iw wpa_supplicant git zsh wipe vim xorg-server xf86-input-evdev xf86-input-synaptics xf86-video-intel android-tools android-udev htop linux-headers dkms i3 xfce4-terminal dmenu sddm networkmanager intel-ucode leafpad mtr nmap net-tools pcmanfm feh wayland openssh  plasma chromium firefox hexchat libreoffice gimp keepnote virtualbox virtualbox-host-modules virtualbox-ext-oracle virtualbox-guest-iso virtualbox-host-dkms qt4 wget axel whois
 wait
 printf "\n\nEnabling services...\n\n"
 systemctl enable sddm.service dkms.service NetworkManager.service && systemctl status dkms.service NetworkManager.service | grep -B4 "Active"
@@ -15,5 +15,5 @@ printf "\n\n"
 wait
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 wait
-printf "\n\nPlease reboot and install nvidia drivers\n\n"
+printf "\n\nplease reboot and install nvidia drivers\n\n"
 exit 0
