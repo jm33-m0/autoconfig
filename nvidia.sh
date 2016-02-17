13123401e9d8053e1a94d5fae4eed681a44e5149
jm33 <jm33.m0@gmail.com>
2016-02-17 21:53:35 +0800

    Minor change

--- nvidia.sh
+++ nvidia.sh
@@ -12,13 +12,13 @@ printf "\n\nSuccessfully installed required packages\n\nNow I'll enable bumblebe
 systemctl enable bumblebeed.service
 systemctl status bumblebeed.service | grep -A4 "Active"
 printf "\n\n"
-gpasswd -a $USER bumblebee
+gpasswd -a $(echo $USER) bumblebee
 read -r -p "Do you want me to reboot for you? [y/n]" response
 if [[ $response =~ [yY](es)* ]];
 then
-    echo "See you soon ..."
+    printf "\n\nSee you soon ..."
     reboot
 else
-    echo "Bye"
+    printf "\n\nBye"
     exit 0
 fi
