aade7fa979caf49a42c3981129fe925dee79f4ef
jm33-m0 <jm33.m0@gmail.com>
2018-05-18 17:14:24 +0800

    update ss install script

--- install_ss.sh
+++ install_ss.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
-dot_repo="https://github.com/m13253/dns-over-https.git"
 ss_config="https://raw.githubusercontent.com/jm33-m0/autoconfig/master/ss_config.tgz"
 ss_service="https://raw.githubusercontent.com/jm33-m0/autoconfig/master/ss-redir@.service"
-dot_config="https://raw.githubusercontent.com/jm33-m0/autoconfig/master/doh-client.conf"
+dot_archive="https://raw.githubusercontent.com/jm33-m0/autoconfig/master/dot.tgz"
 
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 END='\033[0m'
 
 check_root() {
-    if [ ! $(id -u) -eq 0 ]; then
+    if [ ! "$(id -u)" -eq 0 ]; then
         echo -e "$RED[-] You must be root$END"
         exit 1
     fi
 }
 
 install_ss() {
-    sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
-    sudo apt update
-    sudo apt -t stretch-backports install shadowsocks-libev -y
+    if [ "$(uname -m)" = "x86_64" ]; then
+        sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
+        sudo apt update
+        sudo apt -t stretch-backports install shadowsocks-libev -y
+    else
+        apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev libsodium-dev
+        git clone https://github.com/shadowsocks/shadowsocks-libev.git
+        cd shadowsocks-libev || return
+        ./autogen.sh && ./configure && make
+        make install
+    fi
 }
 
 install_dot() {
-    echo -e "$YELLOW[*] make sure you have installed Go and configured GOPATH correctly$END"
-    if [ -z $GOPATH ]; then
-        echo -e "$RED[-] GOPATH not set, can't proceed$END"
-        exit 1
-    fi
-    git clone $dot_repo dot && \
-        cd dot
-    make && make install
+    echo -e "$YELLOW[*] Installing DNSOverHTTPS$END"
+    curl -fLo dot.tgz $dot_archive &&
+        tar xvzpf dot.tgz
+    cd dot || return
+    make install
 }
 
 dns_config() {
@@ -38,15 +42,13 @@ dns_config() {
 
     # dnsmasq service
     apt-get install dnsmasq -y
-    grep "server=127.0.0.1#5353" /etc/dnsmasq.conf > /dev/null
-    if [ ! $? -eq 0 ]; then
-        echo -e "server=127.0.0.1#5353" >> /etc/dnsmasq.conf
+    if ! grep "server=127.0.0.1#53535" /etc/dnsmasq.conf >/dev/null 2>&1; then
+        echo -e "server=127.0.0.1#53535" >>/etc/dnsmasq.conf
     fi
     systemctl enable dnsmasq.service
     systemctl restart dnsmasq.service
 
     # dns over https service
-    curl -fLo /etc/dns-over-https/doh-client.conf $dot_config
     systemctl restart doh-client.service
     systemctl enable doh-client.service
 }
@@ -59,19 +61,19 @@ main() {
     apt-get install ipset -y
 
     # ss config under /etc
-    curl -fLo /tmp/ss_config.tgz $ss_config && \
+    curl -fLo /tmp/ss_config.tgz $ss_config &&
         tar xvpf /tmp/ss_config.tgz -C /
 
     # ss service
-    curl -fLo /lib/systemd/system/ss-redir@.service $ss_service && \
+    curl -fLo /lib/systemd/system/ss-redir@.service $ss_service &&
         systemctl daemon-reload
+
+    # get DNS ready
+    dns_config
 
     # start service
     systemctl start ss-redir@config.service
     systemctl enable ss-redir@config.service
-
-    # get DNS ready
-    dns_config
 }
 
 main
