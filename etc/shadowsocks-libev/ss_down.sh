6baa676ef517cf45bea4ded06c6a56bf42492ab0
jm33-m0 <jm33.m0@gmail.com>
2019-05-30 16:19:30 +0800

    for ubuntu servers

--- etc/shadowsocks-libev/ss_down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-iptables -t nat -D OUTPUT -p tcp -j SHADOWSOCKS
-iptables -t nat -D OUTPUT -p udp -j SHADOWSOCKS
-iptables -t nat -F SHADOWSOCKS
-iptables -t nat -X SHADOWSOCKS
-
-ipset destroy chnroute
--- install.sh
+++ install.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
 # by: jm33-ng
-# 2017-06-10
-# change log
-# 2018-03-24 - switch to spacevim
 
-# urls of target scripts and config files
-zsh_url='https://raw.githubusercontent.com/jm33-m0/autoconfig/master/oh-my-zsh.sh'
-sshd_url='https://raw.githubusercontent.com/jm33-m0/autoconfig/master/sshd_config'
-dotmux_url='https://raw.githubusercontent.com/jm33-m0/autoconfig/master/dotmux.tar.gz'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+END='\033[0m'
 
 export GOPATH=/projects/golang
 
-# install curl if no curl is detected
-apt-get update && apt-get install curl -y
+function info() {
+    echo -e "$BLUE [*] $1 $END"
+}
 
-function check_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo '[-] Please run me as root'
+function warning() {
+    echo -e "$YELLOW [!] $1 $END"
+}
+
+function error() {
+    echo -e "$RED [x] $1 $END"
+}
+
+check_root() {
+    if [ ! "$(id -u)" -eq 0 ]; then
+        error "You must be root"
         exit 1
     fi
 }
-
 # install sshd_config
 function sshd_config() {
     check_root
-    echo '[*] Installing ssh_config'
-    curl -kfsSL $sshd_url -o /etc/ssh/sshd_config
+    info 'Installing ssh_config'
+    cp -av conf/sshd_config /etc/ssh/sshd_config
     systemctl restart ssh.service || service sshd restart
 
-    echo '[*] Adding user jm33'
+    info 'Adding user jm33'
     test -e "$HOME/.ssh" || mkdir -p "$HOME/.ssh"
     cat <<EOF >"$HOME/.ssh/authorized_keys"
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDVaXjv5cX1pjgURLBSleYZYK/jQNr+RF1Sdqa9RHQTGaBynfuuESMU/0BGu8BQLo83F4z5MEzmpG8zOkKjJzbE/1cqfgWEK2KOX5O1LlwqbfMg+8IJd0LMhdlY1o7mvV59s6ke9qcosyq3W3aoRXultfBRgE9GrPxyYmuJyQTiM3S6Y0PDC6FvhyfXpj0lKc6J6vJMF8pzEeWbrqdcd/bBAdpp9wonEs3NBFRso4EwrUV+j9nP61/bD1QfkyjGnJxD8ufcE5V0dLS1SLkGW5ilgp/5fagu9Wj9pEHLNTuUO1pl9tbTEFSXvkeNBCrqBCYH7v4fQRlTtI+P/vzWRiuJLabcvJcwBbaJkc9UoqJ6omw0gBonlY4f5PLHA0epx42cCIcU+7TUAqn2gpqQ9ZCFBYV01e4e+uaC2ko1Es6ua8jrIc2OHuaW8n2MsHfbQfuknDg/08fJu6O7We33pzE2Xc4anOivVfM4MSN5U8BJH5AOIhJA84QrH8PSjIalEowqZDXxb+LVkIPhG7wlqr8kw7wq2ZK9WMz+5tT3XBcJYlPWqwwy/5gKjj9JLBwTiDw1NtXM04327ygKaPBKALfSXSXTB7B1rars7KclgKZ1u3elghqy3jB3vVBCStrxue8aiP7EQhDgfBWZLNhoAFURzfGQ5VDSUHtH+L6K8Ha2tw== jm33@jm33.me
@@ -38,45 +43,35 @@ EOF
         mkdir /home/jm33/.ssh &&
         cp ~/.ssh/authorized_keys /home/jm33/.ssh
 
-    echo '[?] Set password for jm33:'
-    passwd jm33
-    echo '[?] Set password for root:'
-    passwd
+    chown jm33:jm33 /home/jm33/.ssh/authorized_keys
 }
 
 function vim_install() {
-    echo '[*] Installing Vim'
-    curl -sLf https://github.com/jm33-m0/vim_dotfiles/raw/master/install.sh | bash
+    info 'Installing Vim'
+    bash scripts/vim-install.sh
 }
 
 function zsh_install() {
-    echo '[*] Installing oh-my-zsh'
-    curl -kfsSL $zsh_url | bash
+    info 'Installing oh-my-zsh'
+    bash scripts/oh-my-zsh.sh
 }
 
 function install_packages() {
     check_root
     mkdir -p /projects/golang
 
-    echo '[*] Installing daily packages'
+    info 'Installing daily packages'
     apt update &&
         apt full-upgrade -y &&
-        apt install -y tmux nmap glances htop iftop build-essential mtr python-dev python3-dev python-pip python-setuptools python3-setuptools python3-pip autoconf automake make cmake clang golang git zsh obfs4proxy aria2 powerline shellcheck llvm clang-format tor
+        apt install -y curl tmux nmap glances htop iftop build-essential mtr python-dev python3-dev python-pip python-setuptools python3-setuptools python3-pip autoconf automake make cmake clang golang git zsh obfs4proxy aria2 powerline shellcheck llvm clang-format tor
 
-    # this might be unavailable in older distros
-    apt install universal-ctags -y
+    add-apt-repository -y ppa:wireguard/wireguard
+    apt update
+    apt install -y wireguard
 
-    # shell format for vim-autofmt
-    go get -u -v mvdan.cc/sh/cmd/shfmt
-
-    # drive sync
-    go get -u -v github.com/odeke-em/drive/cmd/drive
-
-    # python related
-    apt install python3-autopep8 pylint3 -y
-    python2 -m pip install pylint --user
-    mv "$HOME"/.local/bin/pylint "$HOME"/.local/bin/pylint_2
-    ln -s /usr/bin/pylint3 /usr/local/bin/pylint
+    umask 077
+    cd /etc/wireguard &&
+        wg genkey | tee privatekey | wg pubkey >publickey
 }
 
 install_packages
@@ -84,22 +79,32 @@ test -e ~/.vim || vim_install
 test -e ~/.oh-my-zsh || zsh_install
 grep "45672" /etc/ssh/sshd_config || sshd_config
 
+info 'Changing file permissions for jm33...'
 chown -R jm33:jm33 /home/jm33/.*
-chown -R jm33:jm33 /projects/golang
+chown -R jm33:jm33 /projects
+ls -lah /home/jm33
+ls -lah /projects
 
 # dotmux
-echo '[*] Installing tmux config'
-wget "$dotmux_url"
-tar xvpf dotmux.tar.gz -C "$HOME"
+info 'Installing tmux config'
+cp -av conf/.tmux.conf /home/jm33/.tmux.conf
+sudo cp -av conf/.tmux.conf /root/.tmux.conf
 
-echo "[*] Enabling BBR..."
+info "Enabling BBR..."
 grep -i "bbr" /etc/sysctl.conf >/dev/null
 if ! grep -i "bbr" /etc/sysctl.conf >/dev/null 2>&1; then
     echo "net.core.default_qdisc=fq" >>/etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.conf
     sysctl -p
 fi
-echo "[*] Checking BBR..."
+
+info "Checking BBR..."
 sysctl net.ipv4.tcp_available_congestion_control
 sysctl net.ipv4.tcp_congestion_control
 lsmod | grep bbr
+
+# dont forget to set passwords
+warning "Set password for jm33:"
+passwd jm33
+warning 'Set password for root:'
+passwd
